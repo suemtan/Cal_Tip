cd5e70a04ff4ee017fd4201c9d0818e5e59e1d94
Su Tan <sutan@sus-macbook-pro.local>
2018-08-23 14:32:43 -0700

    Initial Commit

---
+++ Cal_Tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cal_Tip
+//
+//  Created by Su Tan on 8/23/18.
+//  Copyright © 2018 Su Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
